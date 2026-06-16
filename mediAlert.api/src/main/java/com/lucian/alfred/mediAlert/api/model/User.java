613c7c965fc97e2634d0508dfc84993ee824ff36
Luciano Alfredo <lucianalfred60@gmail.com>
2026-06-16 15:49:16 +0100

    user model

--- mediAlert.api/src/main/java/com/lucian/alfred/mediAlert/api/model/User.java
+++ mediAlert.api/src/main/java/com/lucian/alfred/mediAlert/api/model/User.java
@@ -11,5 +11,58 @@ public class User {
 	Date creatAt;
 	
 	
+	public User(){
+		
+	}
+	
+	public User(String nome, String email,
+			String password, String telefone) {
+		
+		this.nome = nome;
+		this.email = email;
+		this.password = password;
+		this.telefone = telefone;
+		
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public Date getCreatAt() {
+		return creatAt;
+	}
+	public void setCreatAt(Date creatAt) {
+		this.creatAt = creatAt;
+	}
+	
+	
 	
 }
